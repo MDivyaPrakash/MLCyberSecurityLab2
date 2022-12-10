814a2e452dec66cf1092fdc92395ff4989bc172a
Divya Prakash Manivannan <dippuprakash@gmail.com>
2022-12-10 00:02:04 -0500

    Updating Eval.py accordingly

--- eval.py
+++ eval.py
@@ -2,10 +2,37 @@ import keras
 import sys
 import h5py
 import numpy as np
+import tensorflow as tf
 
 clean_data_filename = str(sys.argv[1])
 poisoned_data_filename = str(sys.argv[2])
-model_filename = str(sys.argv[3])
+model_B_filename = str(sys.argv[3])
+model_BP_filename = str(sys.argv[4])
+
+class G(tf.keras.Model):
+    def __init__(self, B, B_prime):
+        super(G, self).__init__()
+        self.B = B
+        self.B_prime = B_prime
+
+    def predict(self,data):
+        y = np.argmax(self.B(data), axis=1)
+        y_prime = np.argmax(self.B_prime(data), axis=1)
+        tmpRes = np.array([y[i] if y[i] == y_prime[i] else 1283 for i in range(y.shape[0])])
+        res = np.zeros((y.shape[0],1284))
+        res[np.arange(tmpRes.size),tmpRes] = 1
+        return res
+
+    # For small amount of inputs that fit in one batch, directly using call() is recommended for faster execution, 
+    # e.g., model(x), or model(x, training=False) is faster then model.predict(x) and do not result in
+    # memory leaks (see for more details https://www.tensorflow.org/api_docs/python/tf/keras/Model#predict)
+    def call(self,data):
+        y = np.argmax(self.B(data), axis=1)
+        y_prime = np.argmax(self.B_prime(data), axis=1)
+        tmpRes = np.array([y[i] if y[i] == y_prime[i] else 1283 for i in range(y.shape[0])])
+        res = np.zeros((y.shape[0],1284))
+        res[np.arange(tmpRes.size),tmpRes] = 1
+        return res
 
 def data_loader(filepath):
     data = h5py.File(filepath, 'r')
@@ -15,19 +42,26 @@ def data_loader(filepath):
 
     return x_data, y_data
 
+def data_preprocess(x_data):
+    return x_data/255
+
 def main():
-    cl_x_test, cl_y_test = data_loader(clean_data_filename)
+
+    x_test, y_test = data_loader(clean_data_filename)
     bd_x_test, bd_y_test = data_loader(poisoned_data_filename)
 
-    bd_model = keras.models.load_model(model_filename)
+    B = keras.models.load_model(model_B_filename)
+    B_prime = keras.models.load_model(model_BP_filename)
+    bd_model = G(B, B_prime)
+    #bd_model = keras.models.load_model(model_filename)
 
-    cl_label_p = np.argmax(bd_model.predict(cl_x_test), axis=1)
-    clean_accuracy = np.mean(np.equal(cl_label_p, cl_y_test))*100
-    print('Clean Classification accuracy:', clean_accuracy)
-    
+    clean_label_p = np.argmax(bd_model.predict(x_test), axis=1)
+    class_accu = np.mean(np.equal(clean_label_p, y_test))*100
+    print('Classification accuracy on Clean Data is: ', class_accu)
+
     bd_label_p = np.argmax(bd_model.predict(bd_x_test), axis=1)
     asr = np.mean(np.equal(bd_label_p, bd_y_test))*100
-    print('Attack Success Rate:', asr)
+    print('Attack Success Rate for the model chosen is: ', asr)
 
 if __name__ == '__main__':
     main()
